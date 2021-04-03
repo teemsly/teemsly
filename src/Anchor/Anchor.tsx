6d2732cf2bce5deff7ed31c173c21ddb80b4a849
mrsepdiana <firdasepdiana@gmail.com>
2021-04-03 15:58:58 +0700

    add anchor component

---
+++ src/Anchor/Anchor.tsx
@@ -0,0 +1,20 @@
+import React, { SyntheticEvent } from "react";
+import { CommonProps } from "../@types/common";
+
+export interface AnchorProps extends CommonProps {
+  /** The specified path of the url */
+  href?: string;
+  /** The custom element for this component */
+  componentClass?: React.ElementType,
+  /** The main content for component */
+  children?: React.ReactNode,
+  /** On click action */
+  onClick?: (e: SyntheticEvent) => void
+}
+
+const Anchor = (props: AnchorProps) => {
+  const {componentClass: Component = 'a', children, ...rest} = props
+  return <Component {...rest}>{children}</Component>;
+};
+
+export default Anchor;
