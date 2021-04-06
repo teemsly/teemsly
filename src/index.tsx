9b584f78982510a6a05d80219e02ccc7bc3b0835
mrsepdiana <firdasepdiana@gmail.com>
2021-04-06 21:36:52 +0700

    register anchor, header, topbar, page, sidebar, and top navigation

--- src/index.tsx
+++ src/index.tsx
@@ -1,1 +1,10 @@
+// Base
+export { default as Anchor, AnchorProps } from "./Anchor";
 export { default as Button, ButtonProps } from "./Button";
+export { default as Header, HeaderProps } from "./Header";
+export { default as Topbar, TopbarProps } from "./Topbar";
+
+// Layouts
+export { default as Page, PageProps, PageContext } from "./Page";
+export { default as Sidebar, SidebarProps } from "./Sidebar";
+export { default as TopNavigation } from "./TopNavigation";
