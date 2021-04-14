aa55f7bdd75a49ebd578275d701297495ca9ab77
mrsepdiana <firdasepdiana@gmail.com>
2021-04-14 11:12:20 +0700

    add placement attributes

--- src/@types/common.ts
+++ src/@types/common.ts
@@ -18,4 +18,7 @@ export interface WithComponentAs<As extends React.ElementType | string = React.E
 export declare namespace TypeAttributes {
   type Size = 'xs' | 'sm' | 'md' | 'lg' | 'xl'
   type Color = 'red' | 'green' | 'yellow' | 'orange' | 'blue' | 'violet' | 'cyan'
+  type Placement4 = 'top' | 'right' | 'bottom' | 'left'
+  type Placement8 = 'bottomStart' | 'bottomEnd' | 'topStart' | 'topEnd' | 'leftStart' | 'rightStart' | 'leftEnd' | 'rightEnd'
+  type Placement = Placement4 | Placement8 
 }
