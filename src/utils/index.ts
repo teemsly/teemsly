c9ccae14e404556a15670784b8d09655860c68f1
mrsepdiana <firdasepdiana@gmail.com>
2021-04-14 11:16:00 +0700

    register DOMHelper and overlayClose

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,3 +1,5 @@
 export {default as createComponent} from './createComponent'
 export {default as addPrefix, getClassNamePrefix, defaultClassPrefix, setDefaultClassPrefix} from './classPrefix'
 export {default as useClassNames} from './useClassNames'
+export {default as overlayClose} from './overlayClose'
+export {default as DOMHelper} from './DomHelper'
