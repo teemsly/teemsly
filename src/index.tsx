c13e0953ee7a85b22a162348a9eb43f0bc713f7e
mrsepdiana <firdasepdiana@gmail.com>
2021-04-14 11:17:08 +0700

    register content and Dropdown component

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,8 @@
 // Base
 export { default as Anchor, AnchorProps } from "./Anchor";
 export { default as Button, ButtonProps } from "./Button";
+export { default as Content } from "./Content";
+export { default as Dropdown, DropdownProps } from "./Dropdown";
 export { default as Header, HeaderProps } from "./Header";
 export { default as Nav, NavProps } from "./Nav";
 export { default as Topbar, TopbarProps } from "./Topbar";
