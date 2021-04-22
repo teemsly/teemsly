f1aa2f802f12e23405ea0eaf80e81bafcaa74a0e
mrsepdiana <firdasepdiana@gmail.com>
2021-04-22 11:52:55 +0700

    regiter input and input group

--- src/index.tsx
+++ src/index.tsx
@@ -7,6 +7,8 @@ export { default as Dropdown, DropdownProps } from "./Dropdown";
 export { default as Header, HeaderProps } from "./Header";
 export { default as Nav, NavProps } from "./Nav";
 export { default as Topbar, TopbarProps } from "./Topbar";
+export { default as Input, InputProps } from "./Input";
+export { default as InputGroup, InputGroupProps } from "./InputGroup";
 
 // Layouts
 export { default as Page, PageProps, PageContext } from "./Page";
