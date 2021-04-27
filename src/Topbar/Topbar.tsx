4208e1524b2072141a5dc8f986cea896b6d1c7c8
mrsepdiana <firdasepdiana@gmail.com>
2021-04-27 21:25:10 +0700

    add classes for page topbar

--- src/Topbar/Topbar.tsx
+++ src/Topbar/Topbar.tsx
@@ -1,11 +1,20 @@
 import React from "react";
 import { CommonProps } from "../@types/common";
+import { useClassNames } from "../utils";
 
 export interface TopbarProps extends CommonProps {}
 
 const Topbar: React.FC<TopbarProps> = (props: TopbarProps) => {
-  const { children } = props;
-  return <div>{children}</div>;
+  const { classPrefix = "page-topbar", className, children, ...rest } = props;
+
+  const { merge, withClassPrefix } = useClassNames(classPrefix);
+  const classes = merge(className, withClassPrefix());
+
+  return (
+    <div className={classes} {...rest}>
+      {children}
+    </div>
+  );
 };
 
 export default Topbar;
